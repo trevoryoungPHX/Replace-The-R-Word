86d4ee33bb68945ba5ce4a37749e5b4ad1a6ebb3
trevoryoungPHX <tcyoung4@asu.edu>
2017-09-22 09:23:28 -0700

    presentation

--- Replace-The-R-Word-JAVA.js
+++ Replace-The-R-Word-JAVA.js
@@ -2,19 +2,23 @@ $(document).ready(function(){
 
   let key = "83f6f912309fc34eafb4cfcbf2adf517";
 
-  $("#search").click(function(){
+  $("#searchform").submit(function(e){
+    e.preventDefault();//would normally refresh page at submission, this prevents that
     let user_provided_word = $("#scroll").val();
 
     $.get(`http://words.bighugelabs.com/api/2/${key}/${user_provided_word}/json`, function(data){
       console.log(data);
       data = JSON.parse(data);
       let p = document.createElement("p")
-      $(p).text(data.adjective.syn.join(", "));
+      $(p).text(data.adjective.syn.join(" "));
       console.log(data.adjective.syn.join(", "));
-      $("#results").append(p);
+      $("#results").empty().append(p);
         })
   })
 });
+
+
+
 
 // clearbutton
   $("#clear").click(function(){
